35bb2c6c935dc7db05b2489e6593905bae101df9
mhufflep <mhufflep@student.21-school.ru>
2024-02-19 20:33:13 +0500

    Renamed now time function to nowFmtString, added now function that returns time_point

--- include/Time.hpp
+++ include/Time.hpp
@@ -3,8 +3,11 @@
 #include <chrono>
 #include <string>
 
+using time_point = std::chrono::system_clock::time_point;
+
 namespace Time {
     extern const char * format;
 
-    std::string now(const char *fmt = Time::format);
+    std::string nowFmtString(const char *fmt = Time::format);
+    time_point now(void);
 }
--- src/Time.cpp
+++ src/Time.cpp
@@ -2,8 +2,8 @@
 
 const char *Time::format = "%F %T";
 
-std::string Time::now(const char *fmt) {
-    const auto now = std::chrono::system_clock::now();
+std::string Time::nowFmtString(const char *fmt) {
+    const auto now = Time::now();
     const time_t tp = std::chrono::system_clock::to_time_t(now);
 
     // Could be replaced with 'format' in C++20
@@ -13,4 +13,8 @@ std::string Time::now(const char *fmt) {
     strftime(buff, sizeof(buff), fmt, std::localtime(&tp));
 
     return buff;
+}
+
+time_point Time::now(void) {
+    return std::chrono::system_clock::now();
 }
