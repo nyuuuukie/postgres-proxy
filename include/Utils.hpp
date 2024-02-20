f4445a0748d97ec47c70019886bbdd30c1001f89
mhufflep <mhufflep@student.21-school.ru>
2024-02-20 21:36:41 +0500

    Added header to Utils.hpp

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <netdb.h>
 
 // Resolves hostname by calling getaddrinfo and writes the result into sockaddr_in arg.
 int resolveHostname(const std::string &host, struct sockaddr_in *resAddr);
