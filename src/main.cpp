852cdce9ec401c66b006e115a2c49feb38f0df11
mhufflep <mhufflep@student.21-school.ru>
2024-02-22 22:39:03 +0500

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,13 @@ int main(int ac, char **av) {
 
     Args::parse(av);
 
-    // Log << "the first " << "thing logged" << Log.endl;
-    // Log.info() << "the first " << "thing logged" << Log.endl;
-
     Log.setLogDir(Args::logdir);
     Log.fileLogs(true);
+
+    queryLog.setLogDir(Args::logdir);
+    queryLog.setPrefix("query-");
+    queryLog.fileLogs(true);
+
     Globals::server.start();
 
     return 0;
