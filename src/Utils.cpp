fd9e8f135f47d9d42c11f156b96c2467e65c41e9
mhufflep <mhufflep@student.21-school.ru>
2024-02-22 22:43:30 +0500

    NULL -> nullptr

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -2,18 +2,18 @@
 
 int resolveHostname(const std::string &host, struct sockaddr_in *resAddr) {
 
-    addrinfo *lst = NULL;
+    addrinfo *lst = nullptr;
     addrinfo hints = {};
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
 
-    int res = ::getaddrinfo(host.c_str(), NULL, &hints, &lst);
+    int res = ::getaddrinfo(host.c_str(), nullptr, &hints, &lst);
 
     if (res == 0) {
         *resAddr = *(sockaddr_in*)(lst->ai_addr);
     }
 
-    if (lst != NULL) {
+    if (lst != nullptr) {
         ::freeaddrinfo(lst);
     }
 
