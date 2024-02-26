55f6dfa2cab563c1294ed4b73b6b62427286becc
mhufflep <mhufflep@student.21-school.ru>
2024-02-26 17:02:54 +0500

    Output usage message on unknown parameter

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,17 @@
 int main(int ac, char **av) {
     (void)ac;
 
-    Args::parse(av);
+    if (Args::parse(av) < 0) {
+        return 1;
+    }
 
+    // Server logger settings
     Log.setLogDir(Args::logdir);
     Log.fileLogs(true);
 
+    // Query logger settings
     queryLog.setLogDir(Args::logdir);
-    queryLog.setPrefix("query-");
+    queryLog.setFilePrefix("query-");
     queryLog.fileLogs(true);
     queryLog.stdoutLogs(false);
 
